b62f0d3fc29c7f21281afab6ed73591154edbf93
Sarah Kleinman <kleinman.sarah@gmail.com>
2016-05-09 09:50:46 -0400

    fix media_type_video declaration and only display copyright if present

--- app/controllers/nasa_api_controller.rb
+++ app/controllers/nasa_api_controller.rb
@@ -2,5 +2,6 @@ class NasaApiController < ApplicationController
   def index
     @nasa_api = NasaApi.get_api_info
     @media_type_video = @nasa_api['media_type'].eql?('video')
+    @copyright_present = @nasa_api['copyright'].present?
   end
 end
